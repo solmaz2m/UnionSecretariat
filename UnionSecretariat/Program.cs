c4cfc6d18ba106d064aad8e389b44af2ce8456f3
SolmazZm <solmaz.zm@gmail.com>
2025-10-21 12:25:47 +0330

    .

--- UnionSecretariat/Program.cs
+++ UnionSecretariat/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection1")));
 
 var app = builder.Build();
 app.UseRequestLogging();
